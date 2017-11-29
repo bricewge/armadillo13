f7d3fc2db858f94fb123ecfdf075e8ff5fce7c43
Brice Waegeneire <brice.wge@gmail.com>
2017-11-29 22:27:21 +0100

    fix filename with spaces, nad minor other fixes

--- armadillo_geojson.py
+++ armadillo_geojson.py
@@ -10,21 +10,20 @@ import kml2geojson.main as m
 
 
 @click.command(short_help="Convert KML to GeoJSON")
-@click.option('-st', '--style-type', type=click.Choice(m.STYLE_TYPES), 
-  default=None)
-@click.option('-sf', '--style-filename', 
-  default='style.json')
+@click.option('-st', '--style-type', type=click.Choice(m.STYLE_TYPES), default=None)
+@click.option('-sf', '--style-filename', default='style.json')
 @click.argument('kml_path')
 @click.argument('output_dir')
+
 def k2g(kml_path, output_dir, style_type, style_filename):
     """
     Given a path to a KML file, convert it to a a GeoJSON FeatureCollection file and save it to the given output directory.
 
-    If ``--style_type`` is specified, then also build a JSON style file of the given style type and save it to the output directory under the file name given by ``--style_filename``.
+    If ``--style-type`` is specified, then also build a JSON style file of the given style type and save it to the output directory under the file name given by ``--style_filename``.
     """
     m.convert(kml_path, output_dir, False, style_type, style_filename)
-
     output = output_dir + "/" + os.path.splitext(kml_path)[0] + ".geojson"
+    output = re.sub(r' ', '_', output)
     coordinatesInProperties(output)
 
 def coordinatesInProperties(file_path):
@@ -37,7 +36,7 @@ def coordinatesInProperties(file_path):
     for entry in content["features"]:
         entry["properties"]["longitude"] = entry["geometry"]["coordinates"][0]
         entry["properties"]["latitude"] = entry["geometry"]["coordinates"][1]
-    json.dump(content, open(file_path, "w"), indent=4)
+        json.dump(content, open(file_path, "w"), indent=4)
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
